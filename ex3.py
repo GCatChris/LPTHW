fbf1636f0a05d7309521b54a2832013ede9d5538
GCatChris <gcatchris@gmail.com>
2015-01-14 19:20:27 -0800

    Exercise 3

---
+++ ex3.py
@@ -0,0 +1,41 @@
+# prints statement
+print "I will now count my chickens:"
+
+#prints the object, hens, and the number of them
+print "Hens", 25 + 30 / 6
+
+#prints the object, roosters, and the number of them
+print "Roosters", 100 - 25 * 3 % 4
+
+# prints statement
+print "Now I will count my eggs:"
+
+#prints solution for math statement
+print 3+2+1-5+4%2-1/4+6
+
+#prints true or false question
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#prints whether or not previous statement is true or false
+print 3+2<5-7
+
+#prints question, then outputs the solution to the question
+print "What is 3 + 2?", 3+2
+#prints question, then outputs the solution to the question
+print "What is 5 - 7?", 5-7
+
+#prints statement of ignorance
+print "Oh, that's why it's False."
+#prints statement of sarcasm
+print "How about some more."
+
+#prints T/F question, then answers question
+print "Is it greater?", 5>-2
+#prints T/F question, then answers question
+print "Is it greater or equal?", 5>=-2
+#prints T/F question, then answers question
+print "Is it less or equal?", 5<=-2
+
+print "The size of my dong: ", 1000000/3
+
+print 7.0 / 4
